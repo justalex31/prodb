94462eda4f53fe352787a75dd0e3ec8b54a401f1
lehax22 <kleshevnikov_2011@mail.ru>
2018-09-16 22:13:27 +0300

    init

---
+++ ProDB/ProDB/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using ProDB.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProDB.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; } 
+    }
+}
